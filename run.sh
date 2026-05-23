8c0509295161edf797710461c804101241d55daf
mahdikhashan <mahdikhashan1@gmail.com>
2026-05-23 16:56:47 +0200

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python distill_llama.py --model_config distill_llama3_2_1b_lk_smd_wtk64_fd64_w01 \
+python distill_llama.py --model_config distill_llama3_1_1b_lk_smd_wtk64_fd64_w01 \
   --distill_config distill_alpaca_clean_xent0_mse1000_lr1e-2_1b \
   --finetune_config finetune_lora_qkvo_alpaca_clean_1b \
   --eval_config eval_alpaca_clean \
