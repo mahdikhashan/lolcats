1d835ff84dde8b601f60a7a716d67c0028c2d93d
mahdikhashan <mahdikhashan1@gmail.com>
2026-05-23 17:17:29 +0200

    set run.sh file to be a bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python distill_llama.py --model_config distill_llama3_1_1b_lk_smd_wtk64_fd64_w01 \
   --distill_config distill_alpaca_clean_xent0_mse1000_lr1e-2_1b \
   --finetune_config finetune_lora_qkvo_alpaca_clean_1b \
